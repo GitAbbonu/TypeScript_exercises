12a9ecd10be742e14e4fbaaca4d32a29a95ba414
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-27 19:53:11 +0200

    add:exercise 1

---
+++ typeScript.ts
@@ -0,0 +1,23 @@
+// Add type annotations to each of these variables.
+
+let country: string = "Italy";
+
+let year: undefined | number = undefined;
+
+let averageTemperature: number = 23.6;
+
+let visited: boolean = true;
+
+let currency: null = null;
+
+// Change the type annotations on these variables so they are correct.
+
+let population: number = 47_450_795;
+
+let isSummer: boolean = false;
+
+let languages: string = "Spanish, English";
+
+// ----
+
+export {};
