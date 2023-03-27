5e1d1badb98d66250ad56292662d873e78552b08
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-27 20:13:00 +0200

    add:exercise 2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Esercizi TypeScript
+# Add types to the functions in exercise-2-function-types-start.ts.
---
+++ typeScript.ts
@@ -0,0 +1,42 @@
+// Add types to this function declaration.
+
+function doubleThePopulation(value: number) {
+  return value * 2;
+}
+
+// Correct or remove the function calls which cause type errors.
+
+doubleThePopulation(5);
+
+doubleThePopulation(+"6.12");
+
+doubleThePopulation(8_526);
+
+// doubleThePopulation(false);
+
+// Alter this function so the `language2` parameter is optional.
+// Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
+
+function languagesSpoken(
+  country: string,
+  language1: string,
+  language2?: string
+): void {
+  console.log(`The languages spoken in ${country} are:`);
+
+  console.log(language1);
+
+  if (language2 !== undefined) {
+    console.log(language2);
+  }
+}
+
+languagesSpoken("Colombia", "Spanish", "English");
+
+languagesSpoken("Greece", "Greek");
+
+languagesSpoken("New Zealand", "English", "Māori");
+
+// ----
+
+export {};
