535bd1a017bace5bdc57aba1804d8a394d8ee4d1
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-31 17:24:11 +0200

    add:exercise 14

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Define tuple types for the tuples in exercise-3-tuple-types-start.ts.
+# Fix the initial value for the array reducer in exercise-4-reduce-an-array-start.ts.
--- typeScript.ts
+++ typeScript.ts
@@ -2,26 +2,23 @@
  * START: Follow the instructions below.
  */
 
-// Add a tuple type annotation for this tuple.
+const populations = [1_412_600_000, 60_317_116, 69_950_850, 10_678_632];
 
-const countryPopulation: [string, number] = ["China", 1_412_600_000];
+// The Array reduce method allows us to generate a single value from
+// an array of values. Below we're using it to sum the values of the
+// `populations` array.
+//
+// The `population` parameter in the callback has an inferred type of `number`.
+// Change the initial value (`false`) to a value with the same type as `population`.
+// This should fix the type error on Line 23.
+//
+// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 
-// Change the `CountryWithPopulation` type into a tuple type.
-// This should fix the type error on Line 14.
+const sumOfPopulations = populations.reduce((total, population) => {
+  return total + population;
+}, 0);
 
-type CountryWithCurrency = [];
-
-const countryCurrency: [string, string, string] = ["Italy", "Euro", "EUR"];
-
-// Add a tuple type annotation that prevents this tuple from being modified.
-// Use the readonly modifier and the tuple type syntax: readonly [type, type]
-// This will cause a type error. Remove the code that now has a type error.
-
-const countryLanguage: readonly [string, string] = ["Greece", "Greek"];
-
-// countryLanguage.reverse();
-
-console.log(countryLanguage);
+console.log(sumOfPopulations);
 
 // ----
 
